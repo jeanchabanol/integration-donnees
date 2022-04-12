5dbb95a1cfc6f55b2dc263887fd9a6b61eb3e6a0
etiennb <73204389+etiennb@users.noreply.github.com>
2022-04-12 18:31:22 +0200

    Ca marche !!

--- Aname_Etienne/Aname/index.js
+++ Aname_Etienne/Aname/index.js
@@ -7,37 +7,6 @@ import axios from 'axios';
 const PORT = process.env.PORT || 3000;
 
 
-// const courses =[
-//     {id: 1, name: 'course1'},
-//     {id: 2, name: 'course2'},
-//     {id: 3, name: 'course3'}
-// ]
-
-// app.get('/api/courses', function(req, res) {
-//     response.send('<strong>bienvenue sur mon serveur<strong>');
-// })
-
-// //:name variable
-// //Paramètre qui rest dans l'utilisation de la route
-// //app.get('/nom/:name', function(request, response) {
-// //    response.send('bienvenue ' + request.params.name);
-// //})
-
-// //:name?var1=1&var2=2
-// app.get('/api/courses', function(req, res) {
-//     res.send(courses);
-// })
-
-// app.get('/api/courses/:id', function(req, res) {
-//     const course = courses.find(c => c.id === parseInt(req.params.id));
-//     if (!course) res.status (404).send ('The course with the given ID was not found');
-//     res.send(course);
-// })
-
-// app.listen(PORT, function() {
-//     console.log('Hello :' + PORT);
-// })
-
 
 app.get('/', function(request, response){
 	console.log('Bienvenus')
@@ -45,24 +14,20 @@ app.get('/', function(request, response){
 })
 
 app.get('/Communes', function(request, response){
-	var obj = new Object();
 	
 	axios
 	  .get('https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&rows=1')
 	  .then(res => {
 		console.log(`statusCode: ${res.status}`)
 	    console.log(res)
-
-		obj.a = res['data']['records'][0]['fileds']['nom_de_la_commune']
-		obj.b = res['data']['records'][0]['fileds']['nom_de_la_commune']
-		obj.c = res['data']['records'][0]['fileds']['nom_de_la_commune']
-
-		var jsons = JSON.stringify(obj)
+		var obj = {};
+		obj['Commune'] = res['data']['records'][0]['fields']['nom_de_la_commune'];
+		obj['Code_Insee'] = res['data']['records'][0]['fields']['code_commune_insee']
+		obj['Coord'] = res['data']['records'][0]['geometry']['coordinates']
 
 
 
-
-		response.send(jsons)
+		response.send(obj)
 	})
 
 })
