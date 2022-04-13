4c83e2c332ecb1c5ed6955661c7d265eae749852
Aname  Roumy <anaroumy@gmail.com>
2022-04-13 14:08:14 +0200

    essais boucles

--- Aname_Etienne/Aname/index.js
+++ Aname_Etienne/Aname/index.js
@@ -20,18 +20,30 @@ app.get('/Communes', function(request, response){
 	  .then(res => {
 		console.log(`statusCode: ${res.status}`)
 	    console.log(res)
+		var tab =[]
 		var obj = {};
-		obj['Commune'] = res['data']['records'][0]['fields']['nom_de_la_commune'];
-		obj['Code_Insee'] = res['data']['records'][0]['fields']['code_commune_insee']
-		obj['Coord'] = res['data']['records'][0]['geometry']['coordinates']
+		res['data']['records'].array.forEach(element => {
+			
+			obj['Commune'] = element['fields']['nom_de_la_commune'];
+			obj['Code_Insee'] = element['fields']['code_commune_insee']
+			obj['Coord'] = element['geometry']['coordinates']
+
+			tab= [...tab,obj]
+		})
+	
 
 
 
-		response.send(obj)
+		response.send(tab)
 	})
 
 })
-
+app.get('/Communes:code_insee', function(request, response){
+	
+	response.send('Bienvenue sur mon serveur'+ request.params.code_insee);
+})
+//app.get('/nom/:name', function(request, response) {
+//    response.send('bienvenue ' + request.params.name);
 
 app.listen(PORT, function(){
 	console.log('Bienvenu sur le port :'+ PORT);
