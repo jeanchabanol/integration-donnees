7a68caaad2849abc9efe6160b33d1548c5c2b8f0
etiennb <73204389+etiennb@users.noreply.github.com>
2022-04-13 14:16:49 +0200

    [...TAB]

--- Aname_Etienne/Aname/index.js
+++ Aname_Etienne/Aname/index.js
@@ -14,7 +14,6 @@ app.get('/', function(request, response){
 })
 
 app.get('/Communes', function(request, response){
-	const obj = {};
 	var data = [];
 	axios
 	  .get('https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&rows=12')
@@ -23,8 +22,10 @@ app.get('/Communes', function(request, response){
 		//obj['Commune'] = res['data']['records'][0]['fields']['nom_de_la_commune'];
 		//obj['Code_Insee'] = res['data']['records'][0]['fields']['code_commune_insee']
 		//obj['Coord'] = res['data']['records'][0]['geometry']['coordinates']
-		
+		console.log("nb records", res['data']['records'].length);
 		res['data']['records'].forEach(element =>{
+			const obj = {};
+			console.log("element", element);
 			obj['nom_de_la_commune'] = element['fields']['nom_de_la_commune'];
 			obj['code_commune_insee'] = element['fields']['code_commune_insee']
 			obj['coordinates'] = element['geometry']['coordinates']
